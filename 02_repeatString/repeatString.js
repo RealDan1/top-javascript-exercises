6ed57ff673e771ce360913c6b02207b09355d07a
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-05-08 16:31:42 +0200

    Attempt 1 of repeatstring.js with JEST (WIP)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    for (let i = 1; i < num - 1; i++) {
+        string += string;
+    }
+    return string;
 };
 
 // Do not edit below this line
