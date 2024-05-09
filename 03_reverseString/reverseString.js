6a74cdab1b7f754f9392a0afab3570f938e55211
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-05-09 10:24:09 +0200

    Add Reverse String JEST (Completed)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  stringArr = [];
+  for (let i = 0; i < string.length; i++) {
+    stringArr[i] = string.charAt(i);
+  }
+  let newStringArr = [];
+  for (let i = 0; i < string.length; i++) {
+    newStringArr[string.length - i] = stringArr[i];
+  }
+  const outputString = newStringArr.join('');
+  return outputString;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
