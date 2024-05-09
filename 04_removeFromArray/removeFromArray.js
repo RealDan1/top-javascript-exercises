796acd5c1399aa25d52a7989a9bdb41e48eef4d4
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-05-09 16:30:10 +0200

    Add remove from array first part(WIP) JEST

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (inputs, cut) {
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].includes(cut)) {
+      inputs[i] = inputs[i].splice(i, 1);
+    }
+  }
 };
 
 // Do not edit below this line
