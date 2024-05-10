da158003097f2cd92b8f4f7ebac50a83f72fc5ea
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-05-10 11:14:44 +0200

    Add removeFromArray with REST for variadic function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,14 @@
-const removeFromArray = function (inputs, cut) {
-  for (let i = 0; i < inputs.length; i++) {
-    if (inputs[i].includes(cut)) {
-      inputs[i] = inputs[i].splice(i, 1);
-    }
+const removeFromArray = function (inputs, ...rest) {
+  for (const item of rest) {
+    let atIndexToRemove = inputs.indexOf(item);
+    inputs.splice(atIndexToRemove, 1);
   }
+  console.log(inputs);
+  return inputs;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+//for every item of inputs i need to remove every item of rest
+//so for every input - do remove item[i] of rest
+//for every rest - remove that item from inputs[];
